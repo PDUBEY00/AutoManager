221ed73efed16a777ef1ef08107ade101261020f
n.mathew.joy <n.mathew.joy@CPX-DK192UXVH1O.dir.svc.accenture.com>
2017-05-12 13:02:44 +0100

    Test Classed

---
+++ src/test/java/com/accenture/auto/common/impl/JSONWriterImplTest.java
@@ -0,0 +1,11 @@
+package com.accenture.auto.common.impl;
+
+import org.testng.annotations.Test;
+
+public class JSONWriterImplTest {
+
+  @Test
+  public void writeFile() {
+    throw new RuntimeException("Test not implemented");
+  }
+}
