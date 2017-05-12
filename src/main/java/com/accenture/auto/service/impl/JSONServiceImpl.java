df4192e8ae7010a421983ce172924d857bc666f2
n.mathew.joy <n.mathew.joy@CPX-DK192UXVH1O.dir.svc.accenture.com>
2017-05-12 13:04:09 +0100

    Service class

--- src/main/java/com/accenture/auto/service/impl/JSONServiceImpl.java
+++ src/main/java/com/accenture/auto/service/impl/JSONServiceImpl.java
@@ -1,12 +1,20 @@
 package com.accenture.auto.service.impl;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 
+import com.accenture.auto.common.JSONReader;
+import com.accenture.auto.common.JSONWriter;
 import com.accenture.auto.model.base.Vehicle;
 import com.accenture.auto.service.Service;
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class JSONServiceImpl implements Service{
+	JSONWriter writer;
+	JSONReader reader;
 
 	@Override
 	public HashMap<String, List<Vehicle>> getVehicleDetails() {
@@ -16,8 +24,34 @@ public class JSONServiceImpl implements Service{
 
 	@Override
 	public void saveVehicleDetails(HashMap<String, List<Vehicle>> vehicleDetails) {
-		// TODO Auto-generated method stub
-		
+		ObjectMapper mapper = new ObjectMapper();
+		String jsonString = null;
+		try {
+			jsonString = mapper.writeValueAsString(vehicleDetails);
+		} catch (JsonGenerationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public JSONWriter getWriter() {
+		return writer;
 	}
 
+	public void setWriter(JSONWriter writer) {
+		this.writer = writer;
+	}
+
+	public JSONReader getReader() {
+		return reader;
+	}
+
+	public void setReader(JSONReader reader) {
+		this.reader = reader;
+	}
 }
